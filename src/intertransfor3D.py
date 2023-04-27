de427a3b670f8c61da9d5e268ca85b3221b34b04
AlanDCha <didier.ominguez@gmail.com>
2023-04-27 11:42:55 -0600

    🚧 Linkint rotate front with rotate back

---
+++ src/intertransfor3D.py
@@ -0,0 +1,8 @@
+def rotar3D(choice: int, x: int, y: int, z: int) -> None:
+    pass
+
+def escalar3D():
+    pass
+
+def trasladar3D():
+    pass
--- src/transfor3D.py
+++ src/transfor3D.py
@@ -1,75 +1,39 @@
 from math import sin, cos, pi
 import numpy as np
 
-def rotar3D(points: list[list[float]], piv: list[float], degrees: int) -> list[list[float]]:
+def rotar3D(points: list[list[float]], degX: float, degY: float, degZ: float) -> list[list[float]]:
 
-    radianes = (degrees / pi) * 180
-
+    # * Para el eje Z
+    radianes = (degZ / pi) * 180
     seno = sin(radianes)
     coseno = cos(radianes)
 
-    # * Filling matrix T 
-    # m_T = np.zeros((4, 4))
-    # for i in range():
-    #     if i <= 2:
-    #         m_T[i][3] = - points[0][i]
-    #         m_T[i][i] = 1
-    #     else:
-    #         m_T[i][i] = 1
-    
-    # * Filling matrix T inverse
-    m_T_i = np.linalg.inv(m_T)
+    for i in range(len(points)):
+        aux = points[i].copy()
+        points[i][0] = (aux[0] * coseno) - (aux[1] * seno)
+        points[i][1] = (aux[0] * seno) + (aux[1] * coseno)
 
-    # * Filling matrix Rx
-    m_Rx = np.zeros((4, 4))
-    m_Rx[0][0] = 1
-    m_Rx[3][3] = 1
-    m_Rx[1][1] = coseno
-    m_Rx[1][2] = -seno
-    m_Rx[2][1] = seno
-    m_Rx[2][2] = coseno
-
-    # * Filling matrix Rx inverse
-    m_Rx_i = np.linalg.inv(m_Rx)
-
-    # * Filling matrix Ry
-    m_Ry = np.zeros((4, 4))
-    m_Ry[0][2] = seno
-    m_Ry[2][0] = -seno
-    for i in range(len(m_Ry)):
-        if i % 2 == 0:
-            m_Ry[i][i] = coseno
-        else:
-            m_Ry[i][i] = 1
-    
-    # * Filling matrix Ry inverse
-    m_Ry_i = np.linalg.inv(m_Ry)
-
-    # * Filling matrix Rz
-    m_Rz = np.zeros((4, 4))
-    m_Rz[0][1] = seno
-    m_Rz[1][0] = -seno
-    for i in range(len(m_Rz)):
-        if i <= 1:
-            m_Rz[i][i] = coseno
-        else:
-            m_Rz[i][i] = 1
-
+    # * Para el eje X
+    radianes = (degX / pi) * 180
+    seno = sin(radianes)
+    coseno = cos(radianes)
 
     for i in range(len(points)):
-        for i in range():
-            m_T = np.zeros((4, 4))
-            if i <= 2:
-                m_T[i][3] = - points[0][i]
-                m_T[i][i] = 1
-            else:
-                m_T[i][i] = 1
-        for j in range(len(points)):
-            points
-            aux = m_T_i@m_Rx_i@m_Ry_i@m_Rz@m_Ry@m_Rx@m_T
-    # TODO: Fix these
+        aux = points[i].copy()
+        points[i][1] = (aux[1] * coseno) - (aux[2] * seno)
+        points[i][2] = (aux[1] * seno) + (aux[2] * coseno)
 
+    # * Para el eje Y
+    radianes = (degY / pi) * 180
+    seno = sin(radianes)
+    coseno = cos(radianes)
 
+    for i in range(len(points)):
+        aux = points[i].copy()
+        points[i][0] = (aux[2] * coseno) - (aux[0] * seno)
+        points[i][2] = (aux[2] * seno) + (aux[0] * coseno)
+    
+    return points
 
 def trasladar3D(points: list[list[float]], vector: list[float]) -> list[list[float]]:
     '''Traslada la figura 3D con el vector dado'''

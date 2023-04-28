7988f308e66ea2a1c6b56103e583254fb0e1ffb3
AlanDCha <didier.ominguez@gmail.com>
2023-04-28 11:58:51 -0600

    🎨 Structuring file tree

---
+++ README.md
@@ -0,0 +1,3 @@
+# Transformaciones compuestas 2D y 3D
+
+Estas transformaciones son usando los algoritmos de graficación por computadora
--- src/frontend/dialog.py
+++ src/frontend/dialog.py
@@ -10,29 +10,32 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 import sys
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        self.buttonBox = QtWidgets.QDialogButtonBox(parent=Dialog)
+class Ui_Dialog(QtWidgets.QDialog):
+    def __init__(self):
+
+        super().__init__()
+
+        self.setObjectName("Dialog")
+        self.resize(400, 300)
+        self.buttonBox = QtWidgets.QDialogButtonBox(parent=self)
         self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Orientation.Horizontal)
         self.buttonBox.setStandardButtons(
             QtWidgets.QDialogButtonBox.StandardButton.Cancel | QtWidgets.QDialogButtonBox.StandardButton.Ok)
         self.buttonBox.setObjectName("buttonBox")
-        self.label = QtWidgets.QLabel(parent=Dialog)
+        self.label = QtWidgets.QLabel(parent=self)
         self.label.setGeometry(QtCore.QRect(20, 10, 141, 41))
         font = QtGui.QFont()
         font.setPointSize(14)
         self.label.setFont(font)
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(parent=Dialog)
+        self.label_2 = QtWidgets.QLabel(parent=self)
         self.label_2.setGeometry(QtCore.QRect(20, 60, 141, 41))
         font = QtGui.QFont()
         font.setPointSize(14)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(parent=Dialog)
+        self.label_3 = QtWidgets.QLabel(parent=self)
         self.label_3.setGeometry(QtCore.QRect(30, 150, 341, 51))
         font = QtGui.QFont()
         font.setPointSize(16)
@@ -40,14 +43,14 @@ class Ui_Dialog(object):
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
 
-        self.retranslateUi(Dialog)
-        self.buttonBox.accepted.connect(Dialog.accept)  # type: ignore
-        self.buttonBox.rejected.connect(Dialog.reject)  # type: ignore
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi()
+        self.buttonBox.accepted.connect(self.accept)  # type: ignore
+        self.buttonBox.rejected.connect(self.reject)  # type: ignore
+        QtCore.QMetaObject.connectSlotsByName(self)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.setWindowTitle(_translate("Dialog", "Dialog"))
         self.label.setText(_translate("Dialog", "Hecho por:"))
         self.label_2.setText(_translate("Dialog", "Made by:"))
         self.label_3.setText(_translate(
@@ -56,8 +59,6 @@ class Ui_Dialog(object):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    dialog = Ui_Dialog()
+    dialog.show()
     sys.exit(app.exec())
